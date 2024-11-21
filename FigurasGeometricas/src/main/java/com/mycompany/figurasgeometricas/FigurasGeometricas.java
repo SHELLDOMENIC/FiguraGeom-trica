5ae457816795d294d177675e71b7b165736696f9
ShellDomenic <domenicaloor177@gmail.com>
2024-11-21 14:45:06 -0500

    avance

--- FigurasGeometricas/src/main/java/com/mycompany/figurasgeometricas/FigurasGeometricas.java
+++ FigurasGeometricas/src/main/java/com/mycompany/figurasgeometricas/FigurasGeometricas.java
@@ -11,7 +11,49 @@ package com.mycompany.figurasgeometricas;
 public class FigurasGeometricas {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println("Bienvenidos");
+        System.out.println("Seleccione segun su conveniencia:");
+        switch(menu){
+            case 1:System.out.println("Caso 1: Círculo");
+            float radio;
+            float diametroCirculo;
+            float calcularAreaCirculo;
+            float calcularPerimetroCirculo;
+                System.out.println("Ingrese el Diametro de el circulo");
+                
+            
+            
+            
+                break;
+            case 2:System.out.println("Caso 2: Rectángulo");
+            float base;
+            float alturaRectangulo;
+            float calcularAreaRectangulo;
+            float calcularPerimetroRectangulo;
+            
+            
+                break;
+             case 3:System.out.println("Caso 3: Cuadrado");
+             float ladoCuadrado;
+             float calcularAreaCuadrado;
+             float calcularPerimetroCuadrado;
+             
+                break;
+              case 4:System.out.println("Caso 4: Triángulo");
+              float baseTriangulo;
+              float alturaTriangulo;
+              float calcularAreaTriangulo;
+              float calcularPerimetroTriangulo;
+               
+              
+                break;
+            
+        
+        
+        
+        
+        
+        }
+        
     }
 }
