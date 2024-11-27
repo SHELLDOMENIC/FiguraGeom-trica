403a324fcc315fed88971e698e75b13d05f0ae53
ShellDomenic <domenicaloor177@gmail.com>
2024-11-27 11:18:17 -0500

    susto

--- FigurasGeometricas/src/main/java/com/mycompany/figurasgeometricas/FigurasGeometricas.java
+++ FigurasGeometricas/src/main/java/com/mycompany/figurasgeometricas/FigurasGeometricas.java
@@ -15,10 +15,6 @@ public class FigurasGeometricas {
         System.out.println("Seleccione según su conveniencia:");
         System.out.println("1. Círculo");
         System.out.println("2. Rectángulo");
-        System.out.println("3. Cuadrado");
-        System.out.println("4. Triángulo");
-        System.out.println("5. Salir");
-        
         menu = scanner.nextInt();
         switch(menu){
             case 1:System.out.println("Caso 1: Círculo");
@@ -41,6 +37,15 @@ public class FigurasGeometricas {
             float alturaRectangulo;
             float calcularAreaRectangulo;
             float calcularPerimetroRectangulo;
+                System.out.println("Ingrese la base del rectangulo");
+                baseRectangulo = scanner.nextFloat();
+                System.out.println("Ingrese la altura del Rectangulo");
+                alturaRectangulo = scanner.nextFloat();
+                calcularAreaRectangulo = ( baseRectangulo * alturaRectangulo);
+                calcularPerimetroRectangulo = 2*(baseRectangulo + alturaRectangulo);
+                System.out.println("El Area del Rectangulo es: "+calcularAreaRectangulo);
+                System.out.println("El Perimetro del Rectangulo es : "+calcularPerimetroRectangulo);
+        
         }
         
     }
